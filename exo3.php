fdcc298b5706e6a9085ad88dbf6d4278e5059d41
LepretreA <alex.lepretre@aol.com>
2023-01-20 09:34:51 +0100

    exo3 commit

--- exo3.php
+++ exo3.php
@@ -1,18 +1,29 @@
-//<?php function moyenne($T){
+<?php function moyenne($T){
+$result = array_sum($T)/count($T); 
+return $result;
+}
+ 
 
-//for(int $i =0;$i<$T;$i++){
-//$result = array_sum($T)/count($T); 
-//}
-//return $result;
-//}?>
-
-
-
-<?php 
-$Table=array(6,8,15,46,3,5,5,9,2,1);
-for (int $i=0;$i<$Table;$i++){
-    echo $Table[i];
+$Table=array(
+    0=>6,
+    1=>8,
+    2=>15,
+    3=>46,
+    4=>3,
+    5=>5,
+    6=>5,
+    7=>9,
+    8=>2,
+    9=>1);
+for ($i=0;$i<sizeof($Table);$i++){
+    echo $Table[$i];
     echo "<br>";
 }
-//moyenne($Table);
+echo "<br>";
+$x =  moyenne($Table);
+echo "La moyenne du Tableau est :" .$x;
+
+echo "<br>";
+echo "<br>";
+echo "Let's GO";
 ?>
